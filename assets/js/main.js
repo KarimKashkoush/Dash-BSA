229f3a05807922c4dcb52cfc28806bdaa4e7dc22
Karim Mohamed Kashkoush <124387506+KarimKashkoush@users.noreply.github.com>
2024-08-11 11:23:18 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,9 +50,9 @@ function WhatsApp() {
     let name = document.getElementById("name");
 
     let link = document.getElementById("WhatsApp");
-    link.href = `http://wa.me/+20${inputNumber.value}/?text=انا ${name.value} من BSA، حضرتك سجلت معايا في Bio verse  Bm-Egypt verse فحابين نأكد علي حضرتك الحضور 
-ويرجي الانضمام سريعا الي الجروب التالي لمعرفة التفاصيل الخاصة بالحضور
-https://chat.whatsapp.com/HWLQmqrSNBN9XYSCj5Vm7X`
+    link.href = `http://wa.me/+20${inputNumber.value}/?text=انا ${name.value} من BSA، حضرتك سجلت معانا في Bio verse  Bm-Egypt verse فحابين نأكد علي حضرتك الحضور 
+ ويرجي الانضمام سريعا الي الجروب التالي لمعرفة التفاصيل الخاصة بالحضور
+ https://chat.whatsapp.com/HWLQmqrSNBN9XYSCj5Vm7X`
 }
 
 let date = new Date()
